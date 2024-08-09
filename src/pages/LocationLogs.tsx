76127808ab77c9f6d90ebc9b245aa299029d17f3
MFahad11 <fahadahad567@gmail.com>
2024-08-09 10:06:20 +0500

    bug resolved

--- src/pages/LocationLogs.tsx
+++ src/pages/LocationLogs.tsx
@@ -12,6 +12,8 @@ import { isPending } from '@reduxjs/toolkit';
 import DatePicker from 'react-flatpickr';
 import { set } from 'react-datepicker/dist/date_utils';
 import IconXCircle from '../components/Icon/IconXCircle';
+// import DatePicker from 'react-datepicker';
+// import DateRangePicker from './Forms/DateRangePicker';
 
 
 
@@ -103,8 +105,10 @@ const LocationLogs = () => {
                                 },
                             }}
                             value={selectedDate??new Date()}
-                            className="block w-full p-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="p-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         />
+
+
 
 
 {(
